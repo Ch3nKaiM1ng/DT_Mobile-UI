7d50072a7280fea000fd74e77adaad189ebebc72
yeduxin <1403116015@qq.com>
2019-07-31 11:18:38 +0800

    更新案例

--- src/components/details_case.vue
+++ src/components/details_case.vue
@@ -11,51 +11,52 @@
     <div class="beforetime_user">
       <!-- 案例1 -->
       <div class="CaseOne">
-          <div class="case1">过去的她</div>
-          <div class="case1_Show">
-            <div class="case1_img1">
-              <img src="../\assets\img\index\details_case\DSC_3930@2x.png" alt="">
-            </div>
-            <div class="case1_img2">
-              <img src="../\assets\img\index\details_case\DSC_3941@2x.png" alt="">
-            </div>
-            <div class="case1_img3">
-              <img src="../\assets\img\index\details_case\DSC_3937@2x.png" alt="">
-            </div>
-            <div class="case1_img4">
-              <img src="../\assets\img\index\details_case\oval1@2x.png" alt="">
-              <span>咨询详情</span>
-            </div>
+        <div class="case1">过去的她</div>
+        <div class="case1_Show">
+          <div class="case1_img1">
+            <img src="../\assets\img\index\details_case\DSC_3930@2x.png" alt="">
+          </div>
+          <div class="case1_img2">
+            <img src="../\assets\img\index\details_case\DSC_3941@2x.png" alt="">
+          </div>
+          <div class="case1_img3">
+            <img src="../\assets\img\index\details_case\DSC_3937@2x.png" alt="">
+          </div>
+          <div class="case1_img4">
+            <img src="../\assets\img\index\details_case\oval1@2x.png" alt="">
+            <span>咨询详情</span>
           </div>
         </div>
-        <div class="Case_nav">变美过程</div>
-        <div class="Case">
-          <!-- 案例2  1张图片-->
-          <div class="CaseTwo">
-            <div class="case2_lf">
-              <div class="case2_nav">
-                <img src="../\assets\img\index\details_case\shijianzhou@2x.png" alt="">
-                <span>效果显现期</span>
-                <p>隐形矫正</p>
-              </div>
-              <div class="case2_day">
-                <p>86<span>day</span></p>
-              </div>
+      </div>
+      <div class="Case_nav">变美过程</div>
+      <div class="Case">
+        <!-- 案例2  1张图片-->
+        <div class="CaseTwo">
+          <div class="case2_lf">
+            <div class="case2_nav">
+              <img src="../\assets\img\index\details_case\shijianzhou@2x.png" alt="">
+              <span>效果显现期</span>
+              <p>隐形矫正</p>
             </div>
+            <div class="case2_day">
+              <p>86<span>day</span></p>
+            </div>
+          </div>
           <div class="case2_rf">          
             <div class="case2_img">
               <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
             </div>
             <div class="case2_label">
-              <span>时代天使</span>&nbsp;&nbsp;
-              <span>#韩旭#</span>&nbsp;&nbsp;
+              <div class="case2_lb1">时代天使</div>
+              <div class="case2_lb2">#韩旭#</div>
+              <div class="case2_lb3">#隐形矫正</div>
             </div>
             <div class="case2_conten">
               单颗牙齿磕断并脱落，因为是门牙位置，牙齿缺失会非常影响美观，总给人一种滑稽...
             </div>
           </div>
         </div>
-        <!-- 案例3   2张图片-->
+        <!-- 案例3  2张图片-->
         <div class="CaseThree">
           <div class="case3_lf">
             <div class="case3_nav">
@@ -69,21 +70,20 @@
           </div>
           <div class="case3_rf">          
             <div class="case3_img">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              
+              <div class="case3_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
+              <div class="case3_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
             </div>
             <div class="case3_label">
-              <span>时代天使</span>&nbsp;&nbsp;
-              <span>#韩旭#</span>&nbsp;&nbsp;
+              <div class="case3_lb1">时代天使</div>
+              <div class="case3_lb2">#韩旭#</div>
+              <div class="case3_lb3">#韩旭#</div>
             </div>
             <div class="case3_conten">
-              
               单颗牙齿磕断并脱落，因为是门牙位置，牙齿缺失会非常影响美观，总给人一种滑稽...
             </div>
           </div>
         </div>
-        <!-- 案例4   3-4张图片-->
+        <!-- 案例4  3-4张图片-->
         <div class="CaseFour">
           <div class="case4_lf">
             <div class="case4_nav">
@@ -97,23 +97,21 @@
           </div>
           <div class="case4_rf">          
             <div class="case4_img">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              
+              <div class="case4_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
+              <div class="case4_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
+              <div class="case4_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
             </div>
             <div class="case4_label">
-              <span>时代天使</span>&nbsp;&nbsp;
-              <span>#韩旭#</span>&nbsp;&nbsp;
+              <div class="case4_lb1">时代天使</div>
+              <div class="case4_lb2">#韩旭#</div>
+              <div class="case4_lb3">#韩旭#</div>
             </div>
             <div class="case4_conten">
-              
               单颗牙齿磕断并脱落，因为是门牙位置，牙齿缺失会非常影响美观，总给人一种滑稽...
             </div>
           </div>
         </div>
-        <!-- 案例5   5-6张图片-->
+        <!-- 案例5  5-9张图片-->
         <div class="CaseFive">
           <div class="case5_lf">
             <div class="case5_nav">
@@ -127,26 +125,25 @@
           </div>
           <div class="case5_rf">          
             <div class="case5_img">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
+              <div class="case5_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
+              <div class="case5_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
+              <div class="case5_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
+              <div class="case5_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
+              <div class="case5_img1"><img src="../\assets\img\index\details_case\platinum34@2x.png" alt=""></div>
               
             </div>
             <div class="case5_label">
-              <span>时代天使</span>&nbsp;&nbsp;
-              <span>#韩旭#</span>&nbsp;&nbsp;
+              <div class="case5_lb1">时代天使</div>
+              <div class="case5_lb2">#韩旭#</div>
+              <div class="case5_lb3">#韩旭#</div>
             </div>
             <div class="case5_conten">
-              
               单颗牙齿磕断并脱落，因为是门牙位置，牙齿缺失会非常影响美观，总给人一种滑稽...
             </div>
           </div>
         </div>
-        <!-- 案例6   7-9张图片-->
-        <div class="CaseSix">
+        <!-- 案例6  7-9张图片-->
+        <!-- <div class="CaseSix">
           <div class="case6_lf">
             <div class="case6_nav">
               <img src="../\assets\img\index\details_case\shijianzhou@2x.png" alt="">
@@ -167,18 +164,97 @@
               <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
               <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
               <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
-              <img src="../\assets\img\index\details_case\platinum34@2x.png" alt="">
             </div>
             <div class="case6_label">
               <span>时代天使</span>&nbsp;&nbsp;
               <span>#韩旭#</span>&nbsp;&nbsp;
             </div>
             <div class="case6_conten">
-              
               单颗牙齿磕断并脱落，因为是门牙位置，牙齿缺失会非常影响美观，总给人一种滑稽...
             </div>
           </div>
+        </div> -->
+      </div>
+      <!-- 本例专家 -->
+      <div class="expert">
+        <div class="expert_nav">本例专家</div>
+        <div class="doctor_Show">
+          <div class="doctor_img">
+            <img src="../\assets\img\index\details_case\zjdt2@2x.png" alt="">
+          </div>
+          <div class="expert_details">
+            <div class="expert_name">
+              <img src="../\assets\img\index\details_case\“@2x.png" alt="">
+              <h4>韩旭</h4>
+              <p>职务：正畸技术院长</p>
+            </div>
+            <div class="expert_skill">
+              <div class="expert_skill2">擅长项目</div>
+              <div class="expert_skill3">成年人及青年牙颌畸形传统及隐形矫正、 成人及青少年牙、美国lnvisalign隐隐...</div>
+            </div>
+          </div>
         </div>
+      </div>
+      <!-- 评论 -->
+      <div class="commen_tator">
+        <div class="comment">评论</div>
+        <div class="comment_box">
+          <div class="comment_center">
+            <div class="comment_img">
+              <img src="../\assets\img\index\ask\1a38f67b41f7d3e2723b3250ef681a9ce7c19b181bead-0BCBJh_fw658@2x.png" alt="">
+            </div>
+            <div class="comment_name">
+              <p>白龙</p>
+              <span>2019-6-12</span>
+            </div>
+            <div class="comment_loveimg">
+              <img src="../\assets\img\index\ask\ask_love@2x.png" alt="">
+              <span>1553</span>
+            </div>
+          </div>
+          <h6>可以讲中文吗?</h6>
+        </div>
+        <div class="comment_box">
+          <div class="comment_center">
+            <div class="comment_img">
+              <img src="../\assets\img\index\ask\1a38f67b41f7d3e2723b3250ef681a9ce7c19b181bead-0BCBJh_fw658@2x.png" alt="">
+            </div>
+            <div class="comment_name">
+              <p>白龙</p>
+              <span>2019-6-12</span>
+            </div>
+            <div class="comment_loveimg">
+              <img src="../\assets\img\index\ask\ask_love@2x.png" alt="">
+              <span>1553</span>
+            </div>
+          </div>
+          <h6>可以讲中文吗?</h6>
+        </div>
+        <div class="comment_box2">
+          <div class="comment_center2">
+            <div class="comment_img2">
+              <img src="../\assets\img\index\ask\a66f47c7f8dd5c94cdd260754d51c570a120d03a5d5c5-1tMp7Z_fw658@2x.png" alt="">
+            </div>
+            <div class="comment_name2">
+              <p>月野兔</p>
+              <div id="host">题主</div>
+              <span>2019-6-12</span>
+            </div>
+            <div class="comment_loveimg2">
+              <img src="../\assets\img\index\ask\ask_love@2x.png" alt="">
+              <span>1354</span>
+            </div>
+          </div>
+          <h6>回复<span>@白龙：</span>HAHAHAHA..可以的呀</h6>
+        </div>
+      </div>
+      <!-- 填写评论 -->
+      <div class="reply">
+        <div class="reply_input">
+          <input type="text" placeholder="可以评论哦...">
+          <img src="../\assets\img\index\ask\signature@2x.png" alt="">
+        </div>
+        <div class="reply_comment">评论</div>
       </div>
     </div>
   </div>
